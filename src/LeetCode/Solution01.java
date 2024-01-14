7890987a28ec05b63fc692d17dfc5c137622c2aa
quanli <nuistql@163.com>
2024-01-14 13:51:00 +0800

    两数之和01

--- src/LeetCode/Solution01.java
+++ src/LeetCode/Solution01.java
@@ -1,21 +1,21 @@
 package LeetCode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Solution01 {
     public static void main(String[] args) {
-        int[] arrs ={0,2,3};
-        int[] res = twosum(arrs,5);
-        for(int i : res){
-            System.out.println(i);
-        }
+        int[] nums = {0, 2, 3};
+        int[] res = twoSum(nums, 5);
+        System.out.println(Arrays.toString(res));
     }
-    public static int[] twosum(int[] nums, int target){
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i],i);
-            if(map.containsKey(target - nums[i])){
-                return new int[]{i,map.get(target - nums[i])};
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
             }
         }
         return new int[0];
